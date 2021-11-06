c0d31e8b5520f1e9805861997c40007a071a7347
hardbaited <50992941+hardbaited@users.noreply.github.com>
2021-11-06 04:41:24 +0200

    Update ConfigBlackMarket.lua

--- hard_jobs/Configs/ConfigBlackMarket.lua
+++ hard_jobs/Configs/ConfigBlackMarket.lua
@@ -1,5 +1,5 @@
 ConfigBM = {}
 
 ConfigBM.BmLoc = { 
-    { x = 2839.67, y = -1305.6, z = 45.71, h = 329.38 },
+    { x = 0, y = 0, z = 0, h = 0 },
 } -- Those Change The Location Of The NPCs and the Location of the bm at the same time (you can add as many as u like)
