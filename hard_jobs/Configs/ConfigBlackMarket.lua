8633b718d23abebd380cd8d1943680558bf54831
hardbaited <50992941+hardbaited@users.noreply.github.com>
2021-11-06 04:42:23 +0200

    Delete ConfigBlackMarket.lua

--- hard_jobs/Configs/ConfigBlackMarket.lua
+++
@@ -1,5 +0,0 @@
-ConfigBM = {}
-
-ConfigBM.BmLoc = { 
-    { x = 0, y = 0, z = 0, h = 0 },
-} -- Those Change The Location Of The NPCs and the Location of the bm at the same time (you can add as many as u like)
